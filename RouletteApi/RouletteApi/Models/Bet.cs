ddd17189aceb7d9aad49f894ce5377fde1ba5c42
SebastianCalle <jusemonca@gmail.com>
2020-05-09 02:51:15 -0500

    [Feature]: Create base of API with the models

---
+++ RouletteApi/RouletteApi/Models/Bet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RouletteApi.Models
+{
+    public partial class Bet
+    {
+        public int BetId { get; set; }
+        public string Color { get; set; }
+        public int? Number { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int? RouletteId { get; set; }
+
+        public virtual Roulette Roulette { get; set; }
+    }
+}
---
+++ RouletteApi/RouletteApi/Models/RouletteApiContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RouletteApi.Models
+{
+    public partial class RouletteApiContext : DbContext
+    {
+        public RouletteApiContext()
+        {
+        }
+
+        public RouletteApiContext(DbContextOptions<RouletteApiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Bet> Bet { get; set; }
+        public virtual DbSet<Roulette> Roulette { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RouletteApi;Integrated Security = true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bet>(entity =>
+            {
+                entity.Property(e => e.Color)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.HasOne(d => d.Roulette)
+                    .WithMany(p => p.Bet)
+                    .HasForeignKey(d => d.RouletteId)
+                    .HasConstraintName("FK_Bet_Roulette");
+            });
+
+            modelBuilder.Entity<Roulette>(entity =>
+            {
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
